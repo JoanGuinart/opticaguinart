32632e781c88391ad1a2515ecc9c2d4b4515d265
Joan Guinart Casas <guinartcasasjoan@gmail.com>
2023-03-23 21:08:53 +0100

    otra vez index html

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename='opticaguinart'>
+    <BrowserRouter basename='/opticaguinart'>
     <App />
     </BrowserRouter>
   </React.StrictMode>
