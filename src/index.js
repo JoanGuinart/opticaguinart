dae8a4e8745e2e4bc73dfa54b226b169970348cf
Joan Guinart Casas <guinartcasasjoan@gmail.com>
2023-03-24 12:53:16 +0100

    basename

--- src/index.js
+++ src/index.js
@@ -7,10 +7,9 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
     <App />
     </BrowserRouter>
   </React.StrictMode>
 );
 
-/* basename={process.env.PUBLIC_URL} */
