efe8846cc2d019e9c64de4570f2af3251de1ac5d
Joan Guinart Casas <guinartcasasjoan@gmail.com>
2023-03-24 12:38:27 +0100

    proba de public url fora

--- src/index.js
+++ src/index.js
@@ -7,9 +7,10 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
     <App />
     </BrowserRouter>
   </React.StrictMode>
 );
 
+/* basename={process.env.PUBLIC_URL} */
