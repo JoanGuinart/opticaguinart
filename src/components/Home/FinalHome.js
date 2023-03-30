f341730c16ecad7818287c58c04b2ecde2d8216e
Joan Guinart Casas <guinartcasasjoan@gmail.com>
2023-03-30 14:04:20 +0200

    fucking accent ostia puta

--- src/components/Home/FinalHome.js
+++ src/components/Home/FinalHome.js
@@ -8,8 +8,7 @@ function FinalHome() {
     <div className="divFinalHome">
       <div>
         <h1 className="h11FinalHome">
-          Les ulleres estarán llestes en tan sols 3 pasos. Les tindràs llestes
-          entre 2 i 7 dies.
+          Les ulleres estarán llestes en tan sols 3 pasos
         </h1>
       </div>
       {/* -------------------------------------------------------------------------- */}
@@ -27,7 +26,7 @@ function FinalHome() {
           <img className="image2" src={Noia2} alt="imatgeNoia-2" />
         </div>
         <h1 className="h12FinalHomeR">
-          Escull la muntura que mes t'agradi i s'adapti al teu estil de vida
+          Escull la muntura que més t'agradi i s'adapti al teu estil de vida
         </h1>
       </div>
       {/* -------------------------------------------------------------------------- */}
